121f8bc8c5ca3a045aa372e4b55d210e5fad654f
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:44:29 +0530

    similar main file

---
+++ OOPs/constructor/Parametarized_constructor/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Pen p = new Pen("Reymonds", "Blue", "Ball Pen");
+        p.write();
+        p.refill();
+        p.display();
+    }
+}
