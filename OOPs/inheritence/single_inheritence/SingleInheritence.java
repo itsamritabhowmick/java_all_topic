3a9e6ccbfc46227d846fd4c26d2519f99b32e3cf
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:52:34 +0530

    Main class where methods are call by constructors

---
+++ OOPs/inheritence/single_inheritence/SingleInheritence.java
@@ -0,0 +1,8 @@
+public class SingleInheritence {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.makeSound();
+        Dog d = new Dog();
+        d.makeSound();
+    }
+}
