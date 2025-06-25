0ade26caa88a508115094a9efa14b87c9a45696d
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:27:14 +0530

    First test the code

---
+++ basic/First.java
@@ -0,0 +1,11 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        // System.out.print("Amrita Bhowmick");
+        // System.out.print("Hello World!");
+
+        System.out.println("It moves to the next Line");
+        System.out.println("It moves to the next Line1");
+    }
+}
