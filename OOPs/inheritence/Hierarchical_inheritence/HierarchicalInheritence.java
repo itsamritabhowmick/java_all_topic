aaf08a44515bea7721bd4b94d10e2882ff5ae952
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:58:40 +0530

    Main class

---
+++ OOPs/inheritence/Hierarchical_inheritence/HierarchicalInheritence.java
@@ -0,0 +1,10 @@
+public class HierarchicalInheritence {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        a.makeSound();
+        Dog d = new Dog();
+        d.makeSound();
+        Cat c = new Cat();
+        c.makeSound();
+    }
+}
