df3479fe4db59c3e95c66928ef92de5e90ed5126
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 01:03:05 +0530

    This class implements interfaces I1 and I2

---
+++ OOPs/Abstraction/using_interface/Abs.java
@@ -0,0 +1,12 @@
+public class Abs implements I1, I2 {
+    
+    @Override
+    public void method1() {
+        System.out.println("It is method number 1..");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("It is method number 2.....");
+    }
+}
