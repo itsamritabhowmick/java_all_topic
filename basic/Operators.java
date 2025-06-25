8604b8aab61cd441d829680e17699ae10adf8a27
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:29:37 +0530

    information for different types of Operators

---
+++ basic/Operators.java
@@ -0,0 +1,21 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum of two numbers is: " + (a + b));
+        System.out.println("Sum of two numbers is: " + sum);
+
+        int sub = a - b;
+        System.out.println("Subtraction of two numbers is: " + sub);
+
+        int mul = a * b;
+        System.out.println("Multiplication of two numbers is: " + mul);
+
+        int div = a / b;
+        System.out.println("Division of two numbers is: " + div);
+
+        int mod = a % b;
+        System.out.println("Modulus of two numbers is: " + mod);
+    }
+}
