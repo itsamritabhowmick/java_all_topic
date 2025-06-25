e97ca05f2734f381bf928e0d85cca96023cc0215
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:49:32 +0530

    It is the main file

---
+++ OOPs/Polymorphism/Method_Overriding/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Birds b = new Birds();
+        Crow c = new Crow();
+        b.makeNoise();
+        b.fly();
+        c.makeNoise();
+    }
+}
