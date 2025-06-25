1366bb5542aee195eba1e45b1e75cadc2d529638
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:48:11 +0530

    Example  real-life ... birds

---
+++ OOPs/Polymorphism/Method_Overriding/Birds.java
@@ -0,0 +1,10 @@
+public class Birds {
+
+    public void makeNoise() {
+        System.out.println("Make different tones");
+    }
+
+    public void fly() {
+        System.out.println("Birds are flying");
+    }
+}
