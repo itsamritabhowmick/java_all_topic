4300c1e671360d6656da8e207bb0a0445c869071
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:28:44 +0530

    All about Data types

---
+++ basic/DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int a;
+        a = 45;
+        System.out.println(a + " is Integer type of data");
+
+        float b = 45.62f;
+        System.out.println("Floating Number is :" + b);
+
+        double d = 89.2364512;
+        System.out.println("Double number is: " + d);
+
+        boolean isTrue = true;
+        System.out.println("It is :" + isTrue);
+
+        String s = "hello";
+        System.out.println(s + " is the string");
+
+        char c = 'a';
+        System.out.println(c + " Is the character type");
+    }
+}
