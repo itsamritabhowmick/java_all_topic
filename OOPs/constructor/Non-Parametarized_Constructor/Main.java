6cd18f1e061e492c102ad4f4a615d6f1037a6fe7
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-26 00:43:03 +0530

    Here is the Main file where objects are created and call method using constructors

---
+++ OOPs/constructor/Non-Parametarized_Constructor/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Pen p = new Pen();
+        p.company = "Raymonds";
+        p.color = "Blue";
+        p.type = "BallPen";
+
+        p.write();
+        p.display();
+    }
+}
