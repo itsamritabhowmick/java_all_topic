340e5548ddc6c7bf20e52557bbaf10d9a66768ff
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-27 00:40:44 +0530

    getter and setter methods are used for private data members

---
+++ OOPs/getter_setter/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+
+    // attributes
+    private String name;
+    private int age;
+
+    // using setter method
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    // using getter method
+    public String getName() {
+        return this.name;
+    }
+
+    // using setter method
+    public void setAge(int a) {
+        this.age = a;
+    }
+
+    // using getter method
+    public int getAge() {
+        return this.age;
+    }
+
+}
